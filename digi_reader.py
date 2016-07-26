1fe4f5b179321e41a77501a229746d97de77ecad
Justin Hynes-Bruell <hynes-bruell.j@husky.neu.edu>
2016-07-25 20:15:14 -0400

    ReadMe Update

--- digi_reader.py
+++ digi_reader.py
@@ -1,15 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#
-# ----------------------------------------------------------------------------
-# "THE BEER-WARE LICENSE" (Revision 42):
-# <c.mignanti@gmail.com> wrote this file.  As long as you retain this notice you
-# can do whatever you want with this stuff. If we meet some day, and you think
-# this stuff is worth it, you can buy me a beer in return.
-# ----------------------------------------------------------------------------
-#
-
 import sys
 
 from provider.digikey import *
