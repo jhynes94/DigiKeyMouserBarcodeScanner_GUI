b0d198245257a4709bf9139471526efb123c4f7f
EE Lab <jbruell94@gmail.com>
2016-08-11 10:32:41 -0400

    Fixed compilation bug

--- part.py
+++ part.py
@@ -1,8 +1,45 @@
 class Part:
-   'Part for catalog'
 
-	def __init__(self, barcode):
-		self.barcode = barcode
+    def __init__(self, barcode):
+        self.barcode = barcode
+        self.digiPN = ''
+        self.manfPN = ''
+        self.Description = ''
 
-	def getPart(self):
-		return self.barcode
+	def addDigiPN(self, digiPN):
+		self.digiPN = digiPN
+
+	def addManfPN(self, manfPN):
+		self.manfPN = manfPN
+
+	def addDescription(self, Description):
+		self.Description = Description
+
+	def add_trick(self):
+		pass
+
+    def getPart(self):
+    	try:
+    		print self.barcode
+    	except Exception as e:
+    		print "No barcode"
+        try:
+        	print self.digiPN
+        except Exception as e:
+    		print "No DigiKey PN"
+        try:
+        	print self.manfPN
+        except Exception as e:
+    		print "No Manufacturer Part Number"
+        try:
+        	print self.Description
+        except Exception as e:
+    		print "No Description"
+
+aPart = Part('2345739000000025774934')
+aPart.getPart()
+
+
+
+#aPart.addManfPN('Dizzle-Corp')
+aPart.getPart()
